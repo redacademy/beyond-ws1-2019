64da6e65d266448de5f0101c0ca3e968077e32f2
jose <jmvr37@hotmail.com>
2019-09-05 00:27:28 -0700

    add font awesome

--- themes/beyond-theme/footer.php
+++ themes/beyond-theme/footer.php
@@ -13,7 +13,16 @@
 				<div class="site-info">
 				<div class="footer">
 				<h1>Let's Talk</h1>
+				
 				<button class="submit-btn">Submit</button>
+				<div class="social">
+				<i class="fab fa-facebook-square"></i>
+				<i class="fab fa-instagram"></i>
+				<i class="fab fa-linkedin"></i>
+				<i class="fab fa-twitter-square"></i>
+				</div>
+				<p>© 2019 Amie Peacock.</p>
+				<p>All materials are copyright protected and may not be distributed without the author's express written permission.</p>
 				</div>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
