52bd026f7ed089dc9a065e6d0fd97445df89c55a
jose <jmvr37@hotmail.com>
2019-09-05 20:45:56 -0700

    add svg images to volunteer page

--- themes/beyond-theme/page-volunteer.php
+++ themes/beyond-theme/page-volunteer.php
@@ -9,12 +9,37 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="title-vol">
+			<h1>Join our team</h1>
+			<p>We're looking for exceptional volunteers like you</p>
+			</div>
+			<div class="grid-vol">
+			<div class="grid-container">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/Media/icon/icon-volunteer-fundraising-team-box.svg" alt="fundraising team">
+			<p>Join the fundraising team</p>
+			</div>
+			<div class="grid-container">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/Media/icon/icon-volunteer-fundraising-team.svg" alt="fundraising team">
+			<p>Become a mental health facilitator</p>
+			</div>
+			<div class="grid-container">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/Media/icon/icon-volunteer-directors-team.svg" alt="fundraising team">
+			<p>Join the director's team</p>
+			</div>
+			<div class="grid-container">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/Media/icon/icon-volunteer-organizer-team.svg" alt="fundraising team">
+			<p>Join the event organizer team</p>
+			</div>
+			<div class="grid-container">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/Media/icon/icon-volunteer-social-media-coordinators-team.svg" alt="fundraising team">
+			<p>Join the social media coordinator's team</p>
+			</div>
+			<div class="grid-container">
+			<img src="<?php echo get_stylesheet_directory_uri();?>/Media/icon/icon-volunteer-facilitator.svg" alt="fundraising team">
+			<p>Become a lead facilitator or co-facilitator</p>
+			</div>
+			</div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
