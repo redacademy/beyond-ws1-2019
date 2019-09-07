a4a473b0ed112cbd4422479cebc7730de1592ae2
Roberto H <robhhr@outlook.com>
2019-09-06 21:04:53 -0700

    style header single post

---
+++ themes/beyond-theme/header-single.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * @package Beyond The Conversation
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<div id="page" class="hfeed site">
+			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+
+			<header id="masthead" class="single-header" role="banner">
+				
+				<div class="site-branding">
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				</div><!-- .site-branding -->
+				<div class="header-blog">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'large' ); ?>
+					<?php endif; ?>
+				</div>
+				
+				<nav id="single-navigation" class="single-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</header><!-- #masthead -->
+
+			<div id="content" class="site-content">
--- themes/beyond-theme/template-parts/content-single.php
+++ themes/beyond-theme/template-parts/content-single.php
@@ -9,9 +9,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
