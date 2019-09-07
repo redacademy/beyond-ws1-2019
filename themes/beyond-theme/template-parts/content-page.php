5fe947296048b2736a002463c95c905547409d1c
Miri <mirimarkovitzbl@gmail.com>
2019-09-07 16:57:13 -0700

    work on cfs on the about page.

--- themes/beyond-theme/template-parts/content-page.php
+++ themes/beyond-theme/template-parts/content-page.php
@@ -13,21 +13,19 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		
+	<h2 class="cfs-title"><?php echo CFS()->get( 'title' ); ?></h2>
+	<p class="cfs-description"><?php echo CFS()->get( 'description' ); ?></p>
+	
+		<?php $loop = CFS()->get( 'member' ); ?>
+				<?php foreach ( $loop as $row ) { ?> 
+					<?php echo '<img class="cfs-photo" src="'.$row["photo"].'"/>'; ?>
+					<p class="cfs-name"><?php echo $row['name']; ?></p>
+					<p class="cfs-role"><?php echo $row['role']; ?></p>
+					<p class="cfs-quote"><?php echo $row['quote']; ?></p>
+				<?php }	?>  
+	
 		<?php the_content(); ?>
-		<?php
-			
-			// echo var_dump($loop);
-			// if ($loop !== "" && $row !== "") {
-				$loop = CFS()->get( 'member' );
-				foreach ( $loop as $row ) {
-					echo '<img src="'.$row["photo"].'"/>';
-					echo $row['name'];
-					echo $row['role'];
-					echo $row['quote'];
-				}	
-			// }
-			
-		?>
 		
 		<?php
 			wp_link_pages( array(
