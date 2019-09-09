b3ba7acb5154f38ebf6a1372ce07bfeccff634cf
Roberto H <robhhr@outlook.com>
2019-09-09 02:30:57 -0700

    add linear gradient style to footer

--- themes/beyond-theme/footer.php
+++ themes/beyond-theme/footer.php
@@ -11,21 +11,22 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-				<div class="footer">
-				<h1>Let's Talk</h1>
-				
-				<?php dynamic_sidebar( 'footer-1' ); ?>
+					<div class="footer">
+						<h1>Let's Talk</h1>
+						
+						<?php dynamic_sidebar( 'footer-1' ); ?>
 
-				<button class="submit-btn">Submit</button>
-				<div class="social">
-				<a href="#"><i class="fab fa-facebook-square"></i></a>
-				<a href="#"><i class="fab fa-instagram"></i></a>
-				<a href="#"><i class="fab fa-linkedin"></i></a>
-				<a href="#"><i class="fab fa-twitter-square"></i></a>
-				</div>
-				<p>© 2019 Amie Peacock.</p>
-				<p>All materials are copyright protected and may not be distributed without the author's express written permission.</p>
-				</div>
+						<div class="social">
+							<a href="#"><i class="fab fa-facebook-square"></i></a>
+							<a href="#"><i class="fab fa-instagram"></i></a>
+							<a href="#"><i class="fab fa-linkedin"></i></a>
+							<a href="#"><i class="fab fa-twitter-square"></i></a>
+						</div>
+						<div class="footer-author">
+							<p>© 2019 Amie Peacock.</p>
+							<p>All materials are copyright protected and may not be distributed without the author's express written permission.</p>
+						</div>
+					</div>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
