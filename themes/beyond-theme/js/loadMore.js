3d532c5ca9629a0e257c5986059ef16a64a994bb
Miri <mirimarkovitzbl@gmail.com>
2019-09-13 12:42:44 -0700

    load 2 members each time on the about page, some desktop version style to the about page.

--- themes/beyond-theme/js/loadMore.js
+++ themes/beyond-theme/js/loadMore.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function ($) {
     $('.cfs-member').slice(0, 2).css('display', 'flex');
     $('#loadMore').on('click', function (event) {
         event.preventDefault();
-        $('.cfs-member:hidden').slice(0, 4).slideDown().css('display', 'flex');
+        $('.cfs-member:hidden').slice(0, 2).slideDown().css('display', 'flex');
     });
 })
