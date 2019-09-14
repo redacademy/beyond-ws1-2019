140cfcea52a0086d4c61c77c7ac16e7c3f82916c
jose <jmvr37@hotmail.com>
2019-09-13 17:17:18 -0700

    add class to wordpress front-page

--- themes/beyond-theme/front-page.php
+++ themes/beyond-theme/front-page.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area-front">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?> 
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
