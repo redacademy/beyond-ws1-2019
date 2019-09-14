0673e42eecb8a8eb5fdd05ed7278f7730d2b8b66
jose <jmvr37@hotmail.com>
2019-09-14 13:52:28 -0700

    add front page button

--- themes/beyond-theme/front-page.php
+++ themes/beyond-theme/front-page.php
@@ -23,16 +23,16 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'home' ); ?>
 
 			<?php endif; ?>
-
+			
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content' ); ?>
 
 			<?php endwhile; ?>
-
+			
 			<?php the_posts_navigation(); ?>
-
+		
 		
 
 		</main><!-- #main -->
