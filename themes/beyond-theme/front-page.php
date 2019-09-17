ace7c601a8738415378c2131aa8e010de380066b
jose <jmvr37@hotmail.com>
2019-09-16 23:54:35 -0700

    add responsive blog section front page

--- themes/beyond-theme/front-page.php
+++ themes/beyond-theme/front-page.php
@@ -39,6 +39,8 @@ get_header(); ?>
 			<h2 class="title-front"> <?php echo CFS()->get('title'); ?></h2>
 			<p class="description-front"> <?php echo CFS()->get('description'); ?></p>
 
+			<h3 class="article-title">Recent Article</h3>
+			<div class="cards-content">
 			<?php $args = array(
   			 'order' => 'ASC',
    			'posts_per_page' => 2,
@@ -50,14 +52,16 @@ get_header(); ?>
 			<?php $query = new WP_Query( $args ); /* $args set above*/ ?>
 			<?php if ( $query->have_posts() ) : ?>
 			   <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+			
+			
 			<div class="resources-card">
 			<div class="imgs"> <?php the_post_thumbnail(); ?></div>
-			 
 			  <div class="class-content">
+			  <h1 class="blog-title"><a href='<?php echo esc_url( get_permalink() ) ?>'><?php echo wp_trim_words(get_the_title(), 4, null); ?></a></h1>
+
 	 		 <div class="card-info"> <div class="blog-meta">
 					<span class="posted-on"><?php red_starter_posted_on(); ?></span><span class="posted-by"><?php red_starter_posted_by(); ?></span></div>
 					
-			  <h1 class="blog-title"><a href='<?php echo esc_url( get_permalink() ) ?>'><?php echo wp_trim_words(get_the_title(), 4, null); ?></a></h1>
 			  <div class="comment-card">
 					<i class="fa fa-comment"></i><span class="comment-number"><?php comments_number( '0', '1', '%' ); ?></span>
 				</div>
@@ -67,13 +71,13 @@ get_header(); ?>
 			</div>
 				</div>
 			</div>
-
+		
    			<?php endwhile; ?>
   			 <?php wp_reset_postdata(); ?>
 			<?php else : ?>
       		<h2>Nothing found!</h2>
 			<?php endif; ?>
-			
+			</div>
 			<div class="main-carousel" data-flickity='{ "cellAlign": "right", "contain": true, "wrapAround": true, "prevNextButtons": false, "autoPlay": true}'>
 			
 			<?php $loop = CFS()->get( 'testimonial' ); ?>
