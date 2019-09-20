be0bb19d7c1c8a4d1c8d202d82454bb9bb493cbf
Roberto H <robhhr@outlook.com>
2019-09-19 20:51:34 -0700

    restructure initial search in wp functions

--- themes/beyond-theme/searchform.php
+++ themes/beyond-theme/searchform.php
@@ -1,9 +1,13 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<fieldset class="fieldset">
-		<label id="s-toggle" class="search-toggle" onblur="classBlur()">
-			<!-- <span class="toggle"><input type="search" class="the-search-form" placeholder="Search ..." value="<?php /* echo esc_attr( get_search_query() ); */ ?>" name="s" title="Search for:" required/></span> -->
-			<a class="icon-search-home"><i class="fa fa-search"></i></a>
+	<fieldset>
+		<label>
+			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
+		<button class="search-submit">
+			<span class="icon-search" aria-hidden="true">
+				<i class="fa fa-search"></i>
+			</span>
 			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
+		</button>
 	</fieldset>
 </form>
