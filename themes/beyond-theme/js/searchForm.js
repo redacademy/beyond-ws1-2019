c15c42818e2db431b78630d2886fb59e8d8de79f
Roberto H <robhhr@outlook.com>
2019-09-20 03:34:57 -0700

    position search icon on full-size nav bar

--- themes/beyond-theme/js/searchForm.js
+++ themes/beyond-theme/js/searchForm.js
@@ -1,9 +1,19 @@
 const searchForm = document.getElementsByClassName('search-input')[0];
+const buttonClear = document.getElementById('clear-search');
+
+formClear();
 
 function clearInput() {
     searchForm.value = "";
-    }
+}
+
+function formClear() {
+    buttonClear.addEventListener('click', () => {
+        clearInput();
+    })
+}
 
 window.addEventListener('load', () => {
     clearInput();
 })
+
