56141aa04ebf5c81bb7468d880595448ad945787
Roberto H <robhhr@outlook.com>
2019-09-22 21:05:38 -0700

    style on hover and changing height menu list items

--- themes/beyond-theme/js/navBarColor.js
+++ themes/beyond-theme/js/navBarColor.js
@@ -1,26 +1,43 @@
 jQuery(function($) {
 
-    let navigation = document.getElementsByClassName('main-navigation')[0];
+    const navigation = document.getElementsByClassName('main-navigation')[0];
     const smallFont = document.getElementById('small-font');
     const normalFont = document.getElementById('normal-font');
     const bigFont = document.getElementById('big-font');
+    const menuList = document.querySelectorAll('.menu .menu-item');
+    const menuItems = document.querySelectorAll('.menu li a');
+
     // let $nav = $('header');
     let $win = $(window);
-    let winH = $win.height(); // Get the window height.
 
-    $win.on("scroll", function () {
-        if ($(this).scrollTop() > winH ) {
-            $(navigation).css({ 'background': 'linear-gradient(#fff, #fff, #fff)' });
-            $(smallFont).css({ 'color': '#62284e' });
-            $(normalFont).css({ 'color': '#62284e' });
-            $(bigFont).css({ 'color': '#62284e' });
-        } else {
-            $(navigation).css('background', 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(36, 36, 36, 0.84) 50%, rgba(0, 0, 0, 0) 100%)');
-            $(smallFont).css({ 'color': '#fff' });
-            $(normalFont).css({ 'color': '#fff' });
-            $(bigFont).css({ 'color': '#fff' });
+        if( $(window).width() > 1000) {
+                $win.on("scroll", function () {
+                    if ($(this).scrollTop() > $win.height()) {
+                        $(navigation).css({ 'background': 'linear-gradient(#fff, #fff, #fff)' });
+                        $(smallFont).css({ 'color': '#62284e' });
+                        $(normalFont).css({ 'color': '#62284e' });
+                        $(bigFont).css({ 'color': '#62284e' });
+                        $(menuItems).css({ 'color': '#62284e' });
+                        $(menuList).mouseover(function() {
+                            $(this).css({ 'border-bottom': '2px solid #62284e' });
+                        }).mouseout(function() {
+                            $(this).css({ 'border-bottom': 'none' });
+                        })
+                    } else {
+                        $(navigation).css('background', 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(36, 36, 36, 0.84) 50%, rgba(0, 0, 0, 0) 100%)');
+                        $(smallFont).css({ 'color': '#fff' });
+                        $(normalFont).css({ 'color': '#fff' });
+                        $(bigFont).css({ 'color': '#fff' });
+                        $(menuItems).css({ 'color': '#fff' });
+                        $(menuList).mouseover(function() {
+                            $(this).css({ 'border-bottom': '2px solid #fff' });
+                        }).mouseout(function() {
+                            $(this).css({ 'border-bottom': 'none' });
+                        })
+                    }
+                })
+        
         }
-    }).on("resize", function(){ // If the user resizes the window
-       winH = $(this).height(); // you'll need the new height value
-    });
+    
+
 });
