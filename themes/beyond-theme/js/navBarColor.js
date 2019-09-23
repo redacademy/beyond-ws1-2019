bc723a050d7af4f3803d4844d1551cc7367c3a08
Roberto H <robhhr@outlook.com>
2019-09-22 19:44:55 -0700

    change font resize color on scroll

--- themes/beyond-theme/js/navBarColor.js
+++ themes/beyond-theme/js/navBarColor.js
@@ -11,10 +11,14 @@ jQuery(function($) {
     $win.on("scroll", function () {
         if ($(this).scrollTop() > winH ) {
             $(navigation).css({ 'background': 'linear-gradient(#fff, #fff, #fff)' });
-
+            $(smallFont).css({ 'color': '#62284e' });
+            $(normalFont).css({ 'color': '#62284e' });
+            $(bigFont).css({ 'color': '#62284e' });
         } else {
             $(navigation).css('background', 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(36, 36, 36, 0.84) 50%, rgba(0, 0, 0, 0) 100%)');
-
+            $(smallFont).css({ 'color': '#fff' });
+            $(normalFont).css({ 'color': '#fff' });
+            $(bigFont).css({ 'color': '#fff' });
         }
     }).on("resize", function(){ // If the user resizes the window
        winH = $(this).height(); // you'll need the new height value
