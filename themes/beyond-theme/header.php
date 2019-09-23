ca398ce96e3bde9f5545664c506ad3da0921654e
Roberto H <robhhr@outlook.com>
2019-09-23 00:34:19 -0700

    refactor single post header

--- themes/beyond-theme/header.php
+++ themes/beyond-theme/header.php
@@ -25,25 +25,35 @@
 			<header id="masthead" class="site-header" role="banner">
 			
 				<?php if( is_home() ): ?>
-					<h1 class=text-header-blog><?php echo esc_html( 'Blog' ); ?></h1>
+					<h1 class='text-header-blog'><?php echo esc_html( 'Blog' ); ?></h1>
+					<?php echo '<style type="text/css">
+							.site-header {
+								background: linear-gradient(180deg, rgba(0, 0, 0, .5) 0, rgba(0, 0, 0, .5)), url('.get_stylesheet_directory_uri().'/Media/Images/Blog-Post/hero-blog.jpg);
+								background-position: center;
+								background-size: 100%;
+								background-repeat: no-repeat;
+								width: 100%;
+							}
+					</style>'; ?>
 				<?php elseif( is_post_type_archive() ): ?>
 					<h1 class="text-header">What's On</h1>
 				<?php else: ?>
 					<?php the_title( sprintf( '<h1 class="text-header">', esc_url( get_permalink() ) ), '</h1>' ); ?>
+
+					<?php $background = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' ); ?>
+
+						<style>
+							.site-header {
+								background: linear-gradient(180deg, rgba(0, 0, 0, .5) 0, rgba(0, 0, 0, .5)), url('<?php echo $background[0]; ?>');
+								background-position: center;
+								background-size: 100%;
+								background-repeat: no-repeat;
+								width: 100%;
+							}
+						</style>
+
 				<?php endif ?>
 
-
-				<?php $background = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' ); ?>
-
-				<style>
-					.site-header {
-						background: linear-gradient(180deg, rgba(0, 0, 0, .5) 0, rgba(0, 0, 0, .5)), url('<?php echo $background[0]; ?>');
-						background-position: center;
-						background-size: 100%;
-						background-repeat: no-repeat;
-						width: 100%;
-					}
-				</style>
 
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
