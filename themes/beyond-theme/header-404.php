f8e56769cfae288e6ec69c16d4dab615a8b178b5
Roberto H <robhhr@outlook.com>
2019-09-23 21:56:53 -0700

    style full-width 404 title

--- themes/beyond-theme/header-404.php
+++ themes/beyond-theme/header-404.php
@@ -23,7 +23,7 @@
 			<header id="masthead" class="site-header" role="banner">
 			
 				
-					<h1 class="text-header">404</h1>
+					<h1 class="header-404">404</h1>
 					<div>
 					<p class="text-404">The page you are looking for doesn't exist.</p>
 					</div>
@@ -49,7 +49,7 @@
 						<div class="white-icon">
 							<?php echo '<style type="text/css">
 								.white-icon {
-									background-image: url('.get_stylesheet_directory_uri().'/Media/logo/logo-white.svg) !important;
+									background-image: url('.get_stylesheet_directory_uri().'/Media/logo/logo-white.svg);
 									width: 110px;
 									height: 31px;
 									background-size: 100% !important;
