5ab3249b3d7c21446d5ccebcdd0302c5059ea5ec
Roberto H <robhhr@outlook.com>
2019-09-24 14:49:18 -0700

    fix blog loop on front-page

--- themes/beyond-theme/front-page.php
+++ themes/beyond-theme/front-page.php
@@ -41,44 +41,32 @@ get_header(); ?>
 
 			<div class="hideclass"><a href="<?php echo get_permalink( get_page_by_path( 'blog' ) ) ?>"> <h2>See our blog</h2></a></div>
 			<h3 class="article-title">Recent Article</h3>
+
+
 			<div class="cards-content">
-			<?php $args = array(
-  			 'order' => 'ASC',
-   			'posts_per_page' => 2,
-   			'post_type' => array('post'),
-			);
-			$query = new WP_Query( $args ); ?>
+						<?php
+							$fpblog_loop = new WP_Query( array(
+								'post_type' => 'post',
+								'order' => 'DESC',
+								'posts_per_page' => 2,
+							)); 
+							?>
+
+							<?php if ( $fpblog_loop->have_posts() ) : ?>
+							<?php while ( $fpblog_loop->have_posts() ) : $fpblog_loop->the_post(); ?>
+								
+								<?php get_template_part( 'template-parts/content-home' ); ?>
+
+							<?php endwhile; ?>
+							<?php wp_reset_postdata(); ?>
+
+							<?php else : ?>
+							<p><?php __('No post found'); ?></p>
+						<?php endif; ?>
+			</div>
 
 
-			<?php $query = new WP_Query( $args ); /* $args set above*/ ?>
-			<?php if ( $query->have_posts() ) : ?>
-			   <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-			
-			
-			<div class="resources-card">
-			<div class="imgs"> <?php the_post_thumbnail(); ?></div>
-			  <div class="class-content">
-			  <h1 class="blog-title"><a href='<?php echo esc_url( get_permalink() ) ?>'><?php echo wp_trim_words(get_the_title(), 4, null); ?></a></h1>
 
-	 		 <div class="card-info"> <div class="blog-meta">
-					<span class="posted-on"><?php red_starter_posted_on(); ?></span><span class="posted-by"><?php red_starter_posted_by(); ?></span></div>
-					
-			  <div class="comment-card">
-					<i class="fa fa-comment"></i><span class="comment-number"><?php comments_number( '0', '1', '%' ); ?></span>
-				</div>
-	 		 </div>
-	 		 <div class="read-more">
-			  <?php echo sprintf( '<div class="read-more"><a href="%s" rel="bookmark"><div class="button">', esc_url( get_permalink() )).'Read more <span class="arrow">&gt;</span></div></a></div>'; ?>
-			</div>
-				</div>
-			</div>
-		
-   			<?php endwhile; ?>
-  			 <?php wp_reset_postdata(); ?>
-			<?php else : ?>
-      		<h2>Nothing found!</h2>
-			<?php endif; ?>
-			</div>
 			<div class="main-carousel" data-flickity='{ "cellAlign": "right", "contain": true, "wrapAround": true, "prevNextButtons": true, "autoPlay": true}'>
 			
 			<?php $loop = CFS()->get( 'testimonial' ); ?>
