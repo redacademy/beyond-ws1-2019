44c5a9e8a5adeff59e340eaf961d5ef06f8e2709
Roberto H <robhhr@outlook.com>
2019-09-24 20:20:45 -0700

    add active class to actual page on navbar

--- themes/beyond-theme/functions.php
+++ themes/beyond-theme/functions.php
@@ -175,3 +175,14 @@ function beyond_search_form( $form ) {
     return $form;
 }
 add_filter( 'get_search_form', 'beyond_search_form' );
+
+// Submenu "active" classes
+
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
